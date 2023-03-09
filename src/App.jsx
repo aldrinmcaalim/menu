ee2c61e1b1da901430b979b8f7384f0f7833739a
aldrinmcaalim <aldrinmcaalim@gmail.com>
2023-03-08 19:17:58 -0500

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+import data from "./data";
+
+import Menu from "./components/menu/Menu";
+import Categories from "./components/categories/Categories";
+
+const allCategories = ["all", ...new Set(data.map((item) => item.category))];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [menuItems, setMenuItems] = useState(data);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(data);
+      return;
+    }
+    const newItems = data.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+          <Categories filterItems={filterItems} categories={categories} />
+          <Menu items={menuItems} />
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
